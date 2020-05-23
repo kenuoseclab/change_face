8e5bd69ab76994074dc0d4b74250ac1fb373a033
jinmiluo <1324574369@qq.com>
2020-05-23 14:58:55 +0800

    增加文件

---
+++ src/Config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jinmi
+ * Date: 2020-05-22
+ * Time: 23:57
+ */
+
+namespace App;
+
+class Config
+{
+    const APP_ID = '20016441';
+    const API_KEY = '660ryhjmnfuDmKgtpPRnKzND';
+    const SECRET_KEY = 'FMSH6UwuHUHl3dzIs2YS32XI7vcKyIsj';
+
+}
